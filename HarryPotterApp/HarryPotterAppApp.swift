2e34d7d87602fa38e2ac5acfb1a77f48dbf81c6f
Anton Agafonov <otlichnonick@yandex.ru>
2021-07-21 16:07:03 +0300

    Initial Commit

---
+++ HarryPotterApp/HarryPotterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterAppApp.swift
+//  HarryPotterApp
+//
+//  Created by Anton on 21.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
